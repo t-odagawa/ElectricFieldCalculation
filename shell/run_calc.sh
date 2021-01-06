926bc57f376901330c79dbbfbce7129bed6342f2
Odagawa Takahiro <odagawa.takahiro.57w@st.kyoto-u.ac.jp>
2021-01-06 10:42:35 +0900

    shell modified and document small fix

--- shell/run_calc.sh
+++ shell/run_calc.sh
@@ -1,5 +1,5 @@
-for i in `seq 0 10`;do
-    ncell=`expr 1000 + ${i} \* 100`
+for i in `seq 0 20`;do
+    ncell=`expr 600 + ${i} \* 60`
+    # ncell should be 60 * N (N:integer)
     ./calc_elec 3 ${ncell}
-#   echo ${ncell}
 done
