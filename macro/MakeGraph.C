533a5e2caef8df0dd61ec54f07749572c3d2e9cc
Odagawa Takahiro <odagawa.takahiro.57w@st.kyoto-u.ac.jp>
2021-01-08 02:25:27 +0900

    macro updated to more plots

--- macro/MakeGraph.C
+++ macro/MakeGraph.C
@@ -1,12 +1,16 @@
 void MakeGraph() {
-  const int number_of_graphs = 21;
+  const int number_of_graphs = 26;
   TGraph *g[number_of_graphs];
   TCanvas *c = new TCanvas("c", "c");
+
+  TLegend *l = new TLegend(0.95, 0.55, 0.95, 0.55);
+  l->SetNColumns(2);
   for(int igraph = 0; igraph < number_of_graphs; igraph++) {
     Int_t number_of_cells = 600 +  igraph * 60;
     TString filename = Form("../data/electric_field_3_%d.txt", number_of_cells);
     g[igraph] = new TGraph(filename);
-    g[igraph]->SetMarkerColor(igraph);
+    g[igraph]->SetMarkerColor(igraph+1);
+    l->AddEntry(g[igraph], Form("# of cells = %d", number_of_cells), "p");
     if (igraph == 0) {
       g[igraph]->Draw("A*");
       g[igraph]->GetYaxis()->SetRangeUser(0., 2e5);
@@ -15,5 +19,6 @@ void MakeGraph() {
     else
       g[igraph]->Draw("SAME*");
   }
+  l->Draw();
   c->Update();
 }
